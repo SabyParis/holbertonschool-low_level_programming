e2f4a389735523553f37add942436ca98ef65c09
SabyParis <sabrina-papeau@outlook.com>
2023-02-20 06:54:45 -0800

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilinguak puzzle");
+  return (0);
+}
