7bb721d8ab5e75fde7c3d807c96311ced994b42d
SabyParis <sabrina-papeau@outlook.com>
2023-02-21 04:22:04 -0800

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ Retutrn: Always 0 (success)
+*/
+int main(void)
+{
+  int num;
+  for (num = 0; num < 10; num++)
+    printf("%d", num);
+
+  printf ("\n");
+
+  return (0);
+
+}
