57330b85a3c64404a8e45168a5daf096e75d8c13
SabyParis <sabrina-papeau@outlook.com>
2023-02-22 08:48:22 -0800

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +15,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
 
