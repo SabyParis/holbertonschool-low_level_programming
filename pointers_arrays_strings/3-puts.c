4f8b9efa49059ce01b015f77c24db63107c417e8
SabyParis <sabrina-papeau@outlook.com>
2023-02-27 07:17:57 -0800

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,11 +5,11 @@
  * @str: pointer to the string to print
  */
 void _puts(char *str)
-{	int i;
+{	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+	_putchar(str[i]);
 	}
 	_putchar('\n');
 }
